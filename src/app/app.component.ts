3154bd93721798281846ab67bfb26c30711723aa
Neeraj Singh <neeraj.singh@mindtree.com>
2017-03-06 17:21:30 -0500

    after completing the lesson 2 exercise - simple binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,30 +3,47 @@ import { Component } from '@angular/core';//importing the decorator which I need
 //Using the decorator by the name @Component. 
 @Component({
   selector: 'my-app', //this is what that will be shown in the html tag. it is like css selector as in jquery.
-  template: ` // accessing the property fo the class. The templates are fragments and not complete.
+  template: `
     <h1>{{name}}</h1>
+       
+    <p><i>{{name}}</i> is in {{region}} region.</p>
+    
+    <br/>
+    <button (click)="addressClick()">Show/Hide address</button>
+
+    <div [hidden]="hideAddress">
     <fieldset>
-      <img [src]="image" />
+      <label>Street:</label>{{street}}
     </fieldset>
-    <label [style.color]="color">Favorite Color:</label>{{color}}
-    <button (click)="colorChange()">Toggle Color</button>
-    <select (change)="colorChange($event.target.value)" >
-      <option>red</option>
-      <option>blue</option>
-      <option>green</option>
-    </select>
-    `, 
+    <fieldset>
+      <label>City:</label>{{city}}
+    </fieldset>
+    <fieldset>
+      <label>Region:</label>
+        <select (change)="regionChange($event.target.value)" >
+          <option>East</option>
+          <option>South</option>
+          <option>North</option>
+          <option>West</option>
+        </select>
+    </fieldset>
+    </div>
+    
+    `,
 })
 export class AppComponent  {
-  name = 'Alex Smith';// by default it is a public property.
-  image = 'favicon.ico';
-  color = 'red';
+  name = 'Alexa';
+  street = 'second street';// by default it is a public property.
+  city = 'Anytown';
+  region = '';
+  hideAddress = false;
 
-  clicked() {
-    this.color = this.color == 'red' ? 'blue' : 'red';    
+  //toggles hideAddress property
+  addressClick() {
+    this.hideAddress == false ? this.hideAddress = true : this.hideAddress = false;
   }
 
-  colorChange(color : string) {
-    this.color = color;
+  regionChange(region:string) {
+    this.region = region;
   }
 }
